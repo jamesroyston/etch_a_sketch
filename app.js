8d65b2a3de8c3fc91798013b04ffd5c9e36e09a6
jamesroyston <jamesroyston03@gmail.com>
2018-12-26 19:25:09 -0700

    added grid size changing, press enter to generate new grid with new size, alerts if grid size is larger than 100, finalized style for now

--- app.js
+++ app.js
@@ -6,20 +6,48 @@ const container = document.querySelector('#container')
 
 
 const generateGrid = (length) => {
-  let boxSize = `${100/length}%`;
-  for (let i = 0; i < length*length; i++) {
-    const cell = document.createElement('div')
-    cell.classList.add('cell')
-    container.appendChild(cell)
-    cell.style.width = boxSize
-    cell.style.height = boxSize
-    cell.style.border = '1px lightgrey solid'
-    cell.addEventListener('mouseover', function () {
-      this.style.backgroundColor = 'black'
-      console.log(this)
-    })
+  if (length >= 100) {
+    console.warn('%cSorry, that grid size may cause slowness in your browser!',
+        'color: red; font-size: 36px; font-family: \'Open Sans\'')
+    alert('Sorry, that grid size may cause slowness in your browser.')
+    generateGrid(10)
+  } else {
+    let boxSize = `${100/length}%`;
+    for (let i = 0; i < length*length; i++) {
+      const cell = document.createElement('div')
+      cell.classList.add('cell')
+      container.appendChild(cell)
+      cell.style.width = boxSize
+      cell.style.height = boxSize
+      cell.style.border = '1px lightgrey solid'
+      cell.addEventListener('mouseover', function () {
+        this.style.backgroundColor = 'black'
+      })
+    }
+    DOMstrings.app.appendChild(container)
+
   }
-  DOMstrings.app.appendChild(container)
+
+  
 }
 
-generateGrid(32)
+let gridSize = document.querySelector('#gridInput')
+
+document.querySelector('#submitBtn')
+    .addEventListener('click', function() {
+      container.innerHTML = ''
+      generateGrid(gridSize.value)
+    })
+
+// enter key to submit
+gridSize.addEventListener('keypress', function(e) {
+      if (e.keyCode == 13 || e.which == 13) {
+        console.log(e.keyCode)
+        container.innerHTML = ''
+        generateGrid(gridSize.value)
+      }
+    })
+
+
+// render default grid
+generateGrid(gridSize.value)
